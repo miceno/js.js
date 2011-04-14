e39b439e58c841ea61cd1ca1f8dcc3844dbc9d59
Orestes Sanchez <orestes@acm.org>
2011-04-14 23:53:08 +0000

    updated code

--- arxiu-toc.js
+++ arxiu-toc.js
@@ -1,4 +1,4 @@
-
+/*
 var h2= document.getElementsByTagName( "h2");
 var icariaTitle;
 
@@ -10,8 +10,27 @@ for (var i=0; i< h2.length; i++){
 
 
 var x=0;
+var tableOfContents= [];
 
 for( i=0; i< icariaTitle.length; i++){
     var link = document.createElement( "a");
-    link.setAttribute( "name");
+    link.setAttribute( "name", "x");
+}
+*/
+
+var h2= document.getElementsByTagName( "H2");
+var icariaTitle= [];
+
+for (var i=0; i< h2.length; i++){  
+    console.log( "innerHTML=" + h2[i].innerHTML); 
+    h2[i].setAttribute( "id", "icaria"+i); 
+    icariaTitle[ i]= h2[i].innerHTML;
+    console.log( "title " , icariaTitle[ i]);
+}
+
+
+for( i=0; i< icariaTitle.length; i++){
+    var link = document.createElement( "a");
+    link.setAttribute( "name", icariaTitle[i]);
+    console.log( link);
 }
